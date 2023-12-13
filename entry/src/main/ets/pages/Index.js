79496c423eb281cc63056bcbfb7adab55e4a3721
panwang chang <panwangchang@panwangdeMacBook-Pro.local>
2023-12-13 22:45:05 +0800

    init

---
+++ entry/src/main/ets/pages/Index.js
@@ -0,0 +1,10 @@
+struct Index extends   {
+    constructor() { }
+    build() {
+        .alignItems(VerticalAlign.Center).justifyContent(FlexAlign.Center).height('100%');
+    }
+    onPageShow() {
+    }
+}
+export {};
+//# sourceMappingURL=Index.js.map
